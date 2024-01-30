d722a9608479e2aff8b3251e26fd593108e3f94a
Yan Graim <yangraim@gmail.com>
2024-01-30 10:18:36 -0300

    adição do js na aplicação

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchInput = document.getElementById("search-input");
+const resultArtist = document.getElementById("result-artist");
+const resultPlayList = document.getElementById("result-playlists");
+
+function requestApi(searchTerm) {
+    fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlayList.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+
+}
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlayList.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return;
+    }
+
+    requestApi(searchTerm);
+})
